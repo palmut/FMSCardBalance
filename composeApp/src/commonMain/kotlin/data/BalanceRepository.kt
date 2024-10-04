375df9323573a5a02f6bcb275a5d9398d9559b1b
Danil <danil.t@sumsub.com>
2024-10-04 08:16:37 +0300

    ignore unknown keys

--- composeApp/src/commonMain/kotlin/data/BalanceRepository.kt
+++ composeApp/src/commonMain/kotlin/data/BalanceRepository.kt
@@ -4,15 +4,11 @@ import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.plugins.logging.SIMPLE
-import io.ktor.client.request.accept
 import io.ktor.client.request.get
-import io.ktor.http.ContentType
-import io.ktor.http.contentType
 import io.ktor.http.encodeURLPath
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.coroutines.Dispatchers
@@ -33,7 +29,10 @@ class BalanceRepository {
             level = LogLevel.ALL
         }
         install(ContentNegotiation) {
-            json()
+            json(Json {
+                isLenient = true
+                ignoreUnknownKeys = true
+            })
         }
         install(HttpTimeout) {
             socketTimeoutMillis = 10000
